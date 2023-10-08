d804f86ba2283daf49b6a614ff8c6539baac735a
roq-ai <>
2023-10-08 20:38:02 +0000

    Initial commit

---
+++ src/config/app.config.ts
@@ -0,0 +1,22 @@
+interface AppConfigInterface {
+  ownerRoles: string[];
+  customerRoles: string[];
+  tenantRoles: string[];
+  tenantName: string;
+  applicationName: string;
+  addOns: string[];
+  ownerAbilities: string[];
+  customerAbilities: string[];
+  getQuoteUrl: string;
+}
+export const appConfig: AppConfigInterface = {
+  ownerRoles: ['Business Owner'],
+  customerRoles: [],
+  tenantRoles: ['Business Owner', 'Team Member', 'Customer Service Representative', 'Car Owner'],
+  tenantName: 'Company',
+  applicationName: 'Car Sharing Application',
+  addOns: ['file upload', 'chat', 'notifications', 'file'],
+  customerAbilities: [],
+  ownerAbilities: ['Manage user information', 'Manage company information'],
+  getQuoteUrl: 'https://app.roq.ai/proposal/ea80d24b-bc5d-43c7-a197-fec5cba2a0e1',
+};
